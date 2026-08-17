f7539813746948cbac2adeb0a507a9524ab18686
VEERENDRA-5 <valmikiveerendraprasad55@gmail.com>
2026-08-17 06:08:16 +0530

    font style change

--- todolist/src/components/todoitem/todo.jsx
+++ todolist/src/components/todoitem/todo.jsx
@@ -1,21 +1,22 @@
 import { MdDeleteForever } from "react-icons/md";
 
-import './index.css'
+import "./index.css";
 
-const Todoitem=(props)=>{
-    const {details,deleteItem}=props 
-const {id,text}=details
+const Todoitem = (props) => {
+  const { details, deleteItem } = props;
+  const { id, text } = details;
 
- 
-    return(
-        <div className='item'>
-       
+  return (
+    <div className="item">
+      <div className="text">
+        {" "}
+        <p>{text}</p>
+      </div>
+      <button className="delete" onClick={() => deleteItem(id)}>
+        <MdDeleteForever />
+      </button>
+    </div>
+  );
+};
 
-          <div className='text'> <p >{text}</p></div> 
-<button className=" delete" onClick={()=>deleteItem(id)}><MdDeleteForever/></button>
-
-        </div>
-    )
-}
-
-export default Todoitem
+export default Todoitem;
